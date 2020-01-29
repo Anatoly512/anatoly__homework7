9fd475a014fea0645d2add7096e2f81448432ed3
Anatoly512 <56732684+Anatoly512@users.noreply.github.com>
2020-01-29 14:39:57 +0200

    Start

--- src/Player/Main.java
+++ src/Player/Main.java
@@ -1,10 +1,20 @@
 package Player;
 
-public class Main {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+
+        PlayerMenu playerMenu = new PlayerMenu();
+        playerMenu.playerWindowMenu(primaryStage);
+
+    }
+
 
     public static void main(String[] args) {
-
-
-
+        launch(args);
     }
 }
---
+++ src/Player/PlayerMenu.java
@@ -0,0 +1,12 @@
+package Player;
+
+import javafx.stage.Stage;
+
+public class PlayerMenu {
+
+    public void playerWindowMenu(Stage primaryStage) {
+
+        System.out.println("Start !");
+
+    }
+}
