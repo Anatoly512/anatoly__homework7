bba37ed5e83a4ff9bb22533aa665bb0db0872fca
Anatoly512 <56732684+Anatoly512@users.noreply.github.com>
2020-01-29 14:49:39 +0200

    primary stage done

--- src/Player/PlayerMenu.java
+++ src/Player/PlayerMenu.java
@@ -1,12 +1,28 @@
 package Player;
 
+import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 public class PlayerMenu {
 
+PlayerMenu() {
+
+}
+
     public void playerWindowMenu(Stage primaryStage) {
 
         System.out.println("Start !");
+
+        GridPane root = new GridPane();
+
+
+
+
+
+        primaryStage.setTitle(" Menu ");
+        primaryStage.setScene(new Scene(root, 800, 600));
+        primaryStage.show();
 
     }
 }
