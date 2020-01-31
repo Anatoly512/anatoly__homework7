099b4395c5401594d4c86b645c706929b13d3ea7
Anatoly512 <56732684+Anatoly512@users.noreply.github.com>
2020-01-31 16:40:12 +0200

    pane structure

--- src/Player/PlayerMenu.java
+++ src/Player/PlayerMenu.java
@@ -1,22 +1,40 @@
 package Player;
 
 import javafx.scene.Scene;
-import javafx.scene.layout.GridPane;
+import javafx.scene.control.Button;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
-public class PlayerMenu {
+public class PlayerMenu extends BorderPane {
+
+private Button button;
 
 PlayerMenu() {
-
+    this.button = new Button(" Error ");
 }
 
     public void playerWindowMenu(Stage primaryStage) {
 
-        System.out.println("Start !");
+        System.out.println("Player !");
 
-        GridPane root = new GridPane();
+        BorderPane root = new BorderPane();
 
 
+        DropShadow dropShadow1 = new DropShadow();
+        dropShadow1.setRadius(5.0);
+        dropShadow1.setOffsetX(3.0);
+        dropShadow1.setOffsetY(3.0);
+        dropShadow1.setColor(Color.color(0.4, 0.5, 0.5));
+        button.setEffect(dropShadow1);
+
+        button.setOnAction(e -> {
+           ExceptionProcessing exeption = new ExceptionProcessing();
+           exeption.exceptionWindowShow(primaryStage);  }
+        );
+
+        root.setBottom(button);
 
 
 
